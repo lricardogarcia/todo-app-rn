9ac78e44b97f99ecdfe4ac6d1c50dd4dcaa22780
lricardogarcia <ricardogarcia.py.data@gmail.com>
2026-01-11 19:36:57 -0300

    Modo claro e modo escuro.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,56 @@
+import useTheme from '@/hooks/useTheme';
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+export default function TabsLayout() {
+    const {colors} = useTheme();
+    return (
+        <Tabs
+        screenOptions={{
+            tabBarActiveTintColor: colors.primary,
+            tabBarInactiveTintColor: colors.textMuted,
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: colors.surface,
+                borderTopWidth: 1,
+                borderTopColor: colors.border,
+                height: 90,
+                paddingBottom: 10,
+                paddingTop: 6,
+            },
+            tabBarLabelStyle: {
+                fontSize: 12,
+                fontWeight: '600',
+                marginBottom: 4,
+            }
         }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+        >
+        <Tabs.Screen
+            name="index"
+            options={{
+            title: 'Todos',
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons
+                name="flash-outline"
+                color={color}
+                size={size}
+                />
+            ),
+            }}
+        />
+        <Tabs.Screen
+            name="settings"
+            options={{
+            title: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+                <Ionicons
+                name="settings-outline"
+                color={color}
+                size={size}
+                />
+            ),
+            }}
+        />
+        </Tabs>
+    );
 }
